5168f0563bdd9c2cb2575dde1b5dae230ad9a3c9
tdf5400_PC <1328755427@qq.com>
2019-10-16 12:13:36 +0800

    19.10.16 add threshold image

---
+++ cal_road.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+img_raw = cv2.imread('./0_threshold.png')
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,28 +1,31 @@
 import cv2
 import numpy as np
 
-color_add = {'R': 0, 'G': 68, 'B': 0}
-threshold_value = 0
 
-
-"""
-刷新图像
-"""
 def refresh_img(x=0):
+    """
+    刷新图像
+    """
     x = x
     global img_raw, color_add, threshold_value
     __img = img_raw
     wide, high = __img.shape[0], __img.shape[1]
     # 获取滑动条位置
-    # color_add['R'] = cv2.getTrackbarPos('R', 'Bar') - 255
-    # color_add['G'] = cv2.getTrackbarPos('G', 'Bar') - 255
-    # color_add['B'] = cv2.getTrackbarPos('B', 'Bar') - 255
+    color_add = {'R': 0, 'G': 0, 'B': 0}
+    threshold_value = cv2.getTrackbarPos('threshold', 'Bar')
+    color_add['R'] = cv2.getTrackbarPos('R', 'Bar') - 255
+    color_add['G'] = cv2.getTrackbarPos('G', 'Bar') - 255
+    color_add['B'] = cv2.getTrackbarPos('B', 'Bar') - 255
+
     # 分割图层
     (b, g, r) = cv2.split(__img)
-    add_b = add_g = add_r = np.zeros((wide, high, 1), dtype=np.uint8)
+    add_b = np.zeros((wide, high, 1), dtype=np.uint8)
+    add_g = np.zeros((wide, high, 1), dtype=np.uint8)
+    add_r = np.zeros((wide, high, 1), dtype=np.uint8)
     add_r[:][:] = abs(color_add['R'])
     add_g[:][:] = abs(color_add['G'])
     add_b[:][:] = abs(color_add['B'])
+
     # 单独操作通道
     if color_add['R'] > 0:
         r = cv2.add(r, add_r)
@@ -41,44 +44,22 @@ def refresh_img(x=0):
 
     # 合并通道
     __img = cv2.merge([b, g, r])
-    print(__img[0][0])
-    cv2.imshow('Demo', __img)
-    # # 创建混合图层
-    # color = np.zeros((wide, high, 3), dtype=np.int16)
-    # temp = np.array([np.uint8(color_add['R']), np.uint8(color_add['G']), np.uint8(color_add['B'])], dtype=np.uint8)
-    # color[:][:] = temp
-    # # 原图像与混合图层相加
-    # __img = cv2.add(__img, color)
-    # __img_gray = cv2.cvtColor(__img, cv2.COLOR_RGB2GRAY)
-    # __img_threshold = __img_gray #cv2.threshold(__img_gray, threshold_value, 255, cv2.THRESH_BINARY)[1]
-    # cv2.imshow('Demo', __img_threshold)
-
-
-def threshold_change(x):
-    global threshold_value
-    threshold_value = x
-    refresh_img(0)
+    cv2.imshow('img', __img)
+    # 二值化处理
+    __img_gray = cv2.cvtColor(__img, cv2.COLOR_RGB2GRAY)
+    __img_threshold = cv2.threshold(__img_gray, threshold_value, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow('Changed', __img_threshold)
 
 
 img_raw = cv2.imread('./0.jpg')
-# 转灰度
-img_gray = cv2.cvtColor(img_raw, cv2.COLOR_RGB2GRAY)
-# 二值化
-img_threshold = cv2.threshold(img_gray, threshold_value, 255, cv2.THRESH_BINARY)[1]
-# 自适应二值化
-# img_threshold = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 5)
-cv2.imshow('Demo', img_threshold)
+refresh_img()  # 刷新图像
 
 # 调节窗口
 cv2.namedWindow('Bar')
-cv2.createTrackbar('threshold', 'Bar', 0, 255, threshold_change)
+cv2.createTrackbar('threshold', 'Bar', 0, 255, refresh_img)
 cv2.createTrackbar('R', 'Bar', 255, 510, refresh_img)
-cv2.createTrackbar('G', 'Bar', 255, 511, refresh_img)
-# cv2.createTrackbar('B', 'Bar', 255, 511, refresh_img)
+cv2.createTrackbar('G', 'Bar', 255, 510, refresh_img)
+cv2.createTrackbar('B', 'Bar', 255, 510, refresh_img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-
-
