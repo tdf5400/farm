23508681fdbae5a52af826c917dc3ff39fdb33f8
tdf5400_Surface <1328755427@qq.com>
2019-11-29 18:15:53 +0800

    添加十字法判断，基础部分完成

--- floodFill方法.py
+++ floodFill方法.py
@@ -6,8 +6,10 @@ import time
 log:
 1、【实现竖直方向的seed选取---19.11.08】seed点选取的选择算法：若涂色数量小于某个阈值，向上y像素再次判断，
     直到超过阈值
-2、水平线以上画面置黑，减少干扰（一段时间进行一次判断）
+2、【已实现】水平线以上画面置黑，减少干扰（一段时间进行一次判断）
 3、不同光照条件下自动选取floodfill明暗阈值（通过直方图峰值？）
+4、【实现左右取中值】路径走势计算（边缘走势？十字法？）
+5、动作规划（直线曲率？）
 """
 
 
@@ -26,7 +28,7 @@ def cal_floodFill(image):
     mask = np.zeros([h + 2, w + 2], dtype=np.uint8)  # mask必须行和列都加2，且必须为uint8单通道阵列
 
     # 过高区域不进行处理
-    mask[0:50][:] = 255
+    # mask[0:50][:] = 255
 
     # 计算种子点
     seedThreshold = 20000   # 最少像素值
@@ -37,7 +39,7 @@ def cal_floodFill(image):
 
     while True:
         # floodFill
-        cv2.floodFill(copyImg, mask, tuple(seed), (255, 255, 255), (20,150,255), (20,150,255), flags=cv2.FLOODFILL_FIXED_RANGE)
+        cv2.floodFill(copyImg, mask, tuple(seed), (255, 255, 255), (10,255,255), (60,255,255), flags=cv2.FLOODFILL_FIXED_RANGE)
 
         # 二值化并统计渲染数量
         threImg = cv2.inRange(copyImg, copyImg[seed[1], seed[0]], copyImg[seed[1], seed[0]])    # 将与种子点一样变成白色的点划出来
@@ -54,9 +56,9 @@ def cal_floodFill(image):
                 return None, None
 
     # 形态学运算
-    kernel = np.ones((33, 33), dtype=np.uint8)
+    kernel = np.ones((63, 63), dtype=np.uint8)
     threImg = cv2.morphologyEx(threImg, cv2.MORPH_CLOSE, kernel)
-    # kernel = np.ones((15, 15), dtype=np.uint8)
+    # kernel = np.ones((35, 35), dtype=np.uint8)
     # threImg = cv2.morphologyEx(threImg, cv2.MORPH_OPEN, kernel)
     kernel = np.ones((13, 13), dtype=np.uint8)
     threImg = cv2.morphologyEx(threImg, cv2.MORPH_ERODE, kernel)
@@ -79,29 +81,25 @@ while True:
     time_start = time.perf_counter()
 
     # 获取图像
-    ret, src = camera.read()
-    # src = cv2.imread('./testLib/farm/11.jpg')
+    # ret, src = camera.read()
+    src = cv2.imread('./testLib/camera/16.jpg')
 
     img = cv2.resize(src, (640, 480))
-
     copyImg, threImg = cal_floodFill(img)
-
 
     import roadCal.roadCal as rc
     if threImg is None:
         continue
     threImg = cv2.GaussianBlur(threImg, (53,53), sigmaX=0)
-    line, direct = rc.hough(cv2.Canny(threImg, 100, 127))
+    # line, direct = rc.hough(cv2.Canny(threImg, 100, 127))
 
-    if line is not None:
-        cv2.imshow('line', line)
-        if 30 < direct < 150:
-            print(f'Straight!')
-        else:
-            print(f'Turn!')
-            print(f'{direct}')
+    theta = rc.fitRoad_cross(threImg, 20)
+
+    if theta == 0:
+        print(f'Straight!')
     else:
         print(f'Turn!')
+        print('Theta: ', theta)
 
     cv2.imshow('floodFill', copyImg)
     cv2.imshow('Threshold', threImg)
--- roadCal/roadCal.py
+++ roadCal/roadCal.py
@@ -5,8 +5,120 @@ import math
 """
 路径计算方法整合
 """
-def A():
-    print(111)
+
+def fitRoad_cross(imgThre, threshold):
+    """
+    计算路径（十字法）
+    :param imgThre: 二值化图像
+    :param threshold:  阈值（检测到顶部后，下移距离）
+    :return:曲率数据
+    """
+    output = imgThre.copy()
+    height, width = output.shape[0:2]
+    theta = 0   # 中线斜率，负-左边，正-右边
+
+    # 检测边缘
+    # output = cv2.Canny(output, 1, 120)    # 结果不明显
+    output = cv2.adaptiveThreshold(output, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 0)
+
+    # 从中点向上前进
+    middlePoint = int(width/2)
+    edge = [0, width]   # [左边缘， 右边缘]
+    for i in range(height-1, -1, -1):
+        if output.item((i, middlePoint)) == 255:    # 向上前进遇到边缘
+            startPoint = i + threshold
+            # 向左检测
+            for j in range(middlePoint, -1, -1):
+                if output.item((startPoint, j)) == 255:
+                    edge[0] = j
+                    break
+            # 向右检测
+            for j in range(middlePoint, width):
+                if output.item((startPoint, j)) == 255:
+                    edge[1] = j
+                    break
+
+            # 判断左右开口,取离中点近的值
+            judge = 0   # 0-左，1-右
+            if abs(edge[0] - middlePoint) < abs(edge[1] - middlePoint):
+                judge = 0
+            else:
+                judge = 1
+            # 计算曲率
+            # K = 1/r = (4 * Sabc) / (a*b*c)
+            # Sabc = [(a+b+c)(a+b)(a+c)(b+c)]**0.5
+            A = [height - 1, middlePoint]   # 曲线起点
+            B = [startPoint, edge[judge]]   # 左/右交叉点
+            C = (i, middlePoint)            # 顶点
+
+            # 获取曲线起点【A点】（从两边开始）
+            for j in range(0 if judge==0 else (width - 1), middlePoint, 1 if judge==0 else -1):
+                if output.item(height-1, j) == 255:
+                    A[1] = j
+                    break
+            else:   # 底层找不到则上浮
+                for j in range(height, height-threshold, -1):
+                    for k in range(0 if judge==0 else (width - 1), middlePoint, 1 if judge==0 else -1):
+                        if output.item(height - 1, j) == 255:
+                            A = (j, k)
+                            j = -1  # 退出双重循环
+                            break
+                    if j == -1:
+                        break
+
+            # 边界长度
+            a = ((abs(B[0] - C[0]))**2 + (abs(B[1] - C[1]))**2)**0.5
+            b = ((abs(A[0] - C[0]))**2 + (abs(A[1] - C[1]))**2)**0.5
+            c = ((abs(B[0] - A[0]))**2 + (abs(B[1] - A[1]))**2)**0.5
+
+            Sabc = ((a+b+c)*(a+b)*(a+c)*(b+c))**0.5 # 三角形abc面积
+            K = (4*Sabc) / (a*b*c)  # 曲率
+
+            theta = np.arctan(K)/np.pi*180
+            theta /= 2  # 范围压缩到0-90
+            theta *= -1 if judge == 1 else 1  # 左边空-负 右边空-正
+
+            break
+        else:
+            theta = 0
+
+    return theta
+
+
+
+def fitRoad_middle(imgThre):
+    """
+    计算路径（以每行）
+    :param imgThre: 路径的二值化图像
+    :return: 添加点的图像
+    """
+    output = imgThre.copy()
+    height, width = output.shape[0:2]
+
+    for i in range(height):
+        edge_L = 0
+        edge_R = width - 1
+
+        # 查找左边界
+        for j in range(int(width/2), -1, -1):
+            if output.item((i, j)) == 0:# and output.item((i, j+1)) == 255:
+                edge_L = j  # 记录边界
+                break
+
+        # 查找右边界
+        for j in range(int(width/2), width, 1):
+            if output.item((i, j)) == 0:# and output.item((i, j - 1)) == 255:
+                edge_R = j  # 记录边界
+                break
+
+        # 计算中心
+        middle = int((edge_L + edge_R)/2)
+
+        output.itemset((i, middle), 0)
+
+
+    return output
+
 
 def hough(imgEdge, src=None):
         """
@@ -19,7 +131,7 @@ def hough(imgEdge, src=None):
         if src is None:
             src = np.zeros(imgEdge.shape, dtype=np.uint8)
 
-        lines = cv2.HoughLines(imgEdge, 1, np.pi / 180 * 1, 50)
+        lines = cv2.HoughLines(imgEdge, 1, np.pi / 180 * 1, 19)
         if lines is not None:
             __ground = np.zeros(imgEdge.shape, dtype=np.uint8)
             line_direct_theta = 0  # 记录角度
