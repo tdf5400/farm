be5647f95c5a32851c87d0892859faa7ab26f0fe
tdf5400_PC <1328755427@qq.com>
2019-12-29 20:58:58 +0800

    完成串口控制模块
    Signed-off-by: tdf5400_PC <1328755427@qq.com>

---
+++ Serial.py
@@ -0,0 +1,127 @@
+# -*- coding : utf-8 -*-
+"""
+串口部分
+"""
+
+import serial
+import numpy as np
+import roadCal.roadCal as rc  # 状态协议
+
+ser = None  # 串口对象，使用全局变量方便直接调用函数操作单个串口
+
+ACQUIRE_STA = 0        # 上位机请求状态
+ACQUIRE_BOTH = 1       # 上位机请求状态+状态数据
+
+DATA_HEADER = "aa"      # 数据头
+DATA_FOOTER = "55"      # 数据尾
+
+
+def getPortList():
+    """
+    获取串口列表
+    :return:
+    """
+    import serial.tools.list_ports
+    port_list = list(serial.tools.list_ports.comports())
+    # 输出
+    if len(port_list) == 0:
+        # print('No Port!')
+        return None
+    else:
+        # print('Ports')
+        # for i in range(0, len(port_list)):
+        #     print(port_list[i])
+        return port_list
+
+
+def serialInit(portx, bps, timex):
+    global ser
+    ser = None  # 初始化对象
+    # 类型判断
+    if not (isinstance(portx, str) and isinstance(bps, int)
+            and isinstance(timex, int)):
+        print('serialInit Error!')
+
+    # 打开串口，并得到串口对象
+    ser = serial.Serial(portx, bps, timeout=timex)
+    ser.write(0x11)
+    return ser
+
+
+def readReg():
+    """
+    等待指令
+    :return: 指令信息(ACQUIRE_STA / ACQUIRE_BOTH)整数型
+    """
+    if ser is None:
+        print("[serial]Serial has not Init!")
+        return None
+
+    # 读一行数据（以\r\n划分）
+    temp = bytes.hex(ser.readline())
+
+    if len(temp) > 4:
+        rData = temp[0:-4]  # 去掉末尾的\r\n
+        # print(f'[serial]receive {len(rData)} byte: {rData}')
+
+        # 确认数据头数据尾
+        if rData[0:2] == DATA_HEADER and \
+                rData[-2:] == DATA_FOOTER:
+            reg = rData[2:-2]
+            # print(f'[serial]receive reg:{reg}')
+            return int(reg)     # 返回整数型的数据
+
+    return None
+
+def sendData(Sta, Data=None):
+    """
+    串口发送数据
+    :param Sta: 状态标志(roadCal状态表)
+    :param Data: 状态数据(int16)
+    :return: 成功-1 错误-0
+    """
+    if Data is not None:
+        # 数据加工
+        Data = np.int16(Data)
+
+        if Data > 32767:        # 范围限制
+            Data = 32767
+        elif Data < -32768:
+            Data = -32768
+
+        Data = Data % 65535     # 正负数转换（数据不能超过范围）
+
+        Data = hex(Data)    # 10进制转16进制
+        Data = Data[2:]     # 除去'0x'
+        Data = Data[-4:]
+        while len(Data) < 4:  # 变为4个字符
+            Data = '0' + Data
+
+    Sta = hex(Sta)      # 10进制转16进制
+    Sta = Sta[2:]       # 除去'0x'
+    Sta = Sta[-4:]
+    while len(Sta) < 2: # 变为2个字符
+        Sta = '0' + Sta
+
+    ser.write(bytes.fromhex(DATA_HEADER))   # 首字节
+    ser.write(bytes.fromhex(Sta))           # 状态
+    if Data is not None:                    # 状态数据
+        ser.write(bytes.fromhex(Data[0:2]))
+        ser.write(bytes.fromhex(Data[2:4]))
+    ser.write(bytes.fromhex(DATA_FOOTER))  # 尾字节
+
+    # hexData = bytes.fromhex(Data)
+
+
+"""
+TestCode
+"""
+# ports = getPortList()
+# for i in range(0, len(ports)):
+#     print(ports[i])
+# com = str(input('COM?'))
+# serialInit(com,115200,5)
+serialInit("COM3", 115200, 5)
+# data = readReg()
+sendData(rc.FIT_CROSS_TRUN, 0)
+
--- roadCal/roadCal.py
+++ roadCal/roadCal.py
@@ -6,10 +6,10 @@ import math
 路径计算方法整合
 """
 
-FIT_CROSS_STRAIGHT = 0
-FIT_CROSS_TRUN = 1
-FIT_CROSS_OUT = 2
-FIT_CROSS_ERR = -1
+FIT_CROSS_STRAIGHT = 0  # 直道
+FIT_CROSS_TRUN = 1      # 弯道
+FIT_CROSS_OUT = 2       # 出林道
+FIT_CROSS_ERR = -1      # 错误
 
 
 def fitRoad_cross(imgThre, threshold):
@@ -211,3 +211,63 @@ def hough(imgEdge, src=None):
             # print(f'direct:{line_direct_output}')
             return src, line_direct_output
     return None, None
+
+def cal_floodFill(image):
+    """
+    floodFill 计算地面方法
+    :param image:Any
+    :return: copyImg, threImg
+    """
+    copyImg = cv2.medianBlur(image, 3)
+
+    # BGR转换为HSV进行处理
+    copyImg = cv2.cvtColor(copyImg, cv2.COLOR_BGR2HSV)
+
+    h, w = image.shape[:2]
+    mask = np.zeros([h + 2, w + 2], dtype=np.uint8)  # mask必须行和列都加2，且必须为uint8单通道阵列
+
+    # 过高区域不进行处理
+    # mask[0:50][:] = 255
+
+    # 计算种子点
+    seedThreshold = 20000   # 最少像素值
+    timesLimit = 5         # 计算次数限制
+    seed = [319, 479]       # 以画面中间最下面的点为起始点 （x, y）
+    times = 0               # 循环次数，若超过阈值则返回(None,None)
+    seedMoveDistance = int(seed[1] / timesLimit)    # 失败后上升的距离
+
+    while True:
+        # floodFill
+        # --------------------------------19.12.21 阈值数据备份--------------------------------
+        # cv2.floodFill(copyImg, mask, tuple(seed), (255, 255, 255), (20, 100, 255), (40, 150, 255),
+        #               flags=cv2.FLOODFILL_FIXED_RANGE)
+        # ------------------------------------------------------------------------------------
+        cv2.floodFill(copyImg, mask, tuple(seed), (255, 255, 255), (10, 20, 20), (10, 20, 50),
+                      flags=cv2.FLOODFILL_FIXED_RANGE)
+
+        # 二值化并统计渲染数量
+        threImg = cv2.inRange(copyImg, copyImg[seed[1], seed[0]], copyImg[seed[1], seed[0]])    # 将与种子点一样变成白色的点划出来
+        threCounter = np.sum(threImg == 255)    # 统计出现的数量
+
+        # 退出的判定
+        if threCounter >= seedThreshold:
+            break
+        else:
+            times += 1
+            if times < timesLimit:
+                seed[1] -= seedMoveDistance   # seed上移
+            else:
+                return None, None
+
+    # 形态学运算
+    kernel = np.ones((57, 57), dtype=np.uint8)
+    threImg = cv2.morphologyEx(threImg, cv2.MORPH_CLOSE, kernel)
+    # kernel = np.ones((35, 35), dtype=np.uint8)
+    # threImg = cv2.morphologyEx(threImg, cv2.MORPH_OPEN, kernel)
+    kernel = np.ones((13, 13), dtype=np.uint8)
+    threImg = cv2.morphologyEx(threImg, cv2.MORPH_ERODE, kernel)
+
+    # 色彩空间转换BGR
+    copyImg = cv2.cvtColor(copyImg, cv2.COLOR_HSV2BGR)
+
+    return copyImg, threImg
